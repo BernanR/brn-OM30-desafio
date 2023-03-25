6d70efaf45a47fd11d9d5323fdd16335574d7871
Bernan Ribeiro <alves.bernan@gmail.com>
2023-03-25 13:16:05 -0300

    criação das migrates

---
+++ application/database/migrations/2023_03_25_152037_create_pacientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_completo');
+            $table->string('nome_mae_completo');
+            $table->date('data_nascimento');
+            $table->string('foto');
+            $table->integer('cpf')->unique()->unsigned();
+            $table->integer('cns')->unique()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
